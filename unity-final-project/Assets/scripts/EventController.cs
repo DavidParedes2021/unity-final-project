beec473c0bf8ac2a32aac70da7b4f1ef2b8ff913
Lacia7u7 <carlosernestoone@gmail.com>
2023-05-18 09:21:34 -0600

    More Scripts

--- unity-final-project/Assets/scripts/EventController.cs
+++ unity-final-project/Assets/scripts/EventController.cs
@@ -1,5 +1,16 @@
-﻿using UnityEngine;
-public class EventController
+﻿using System.Collections.Generic;
+using UnityEngine;
+public class EventController : MonoBehaviour
 {
-        
+        private UiController _uiController;
+        private List<Zombie> _zombies;
+        private MainPlayer _mainPlayer;
+        private List<Consumable> _consumibles;
+        private List<Weapon> _weapons;
+
+        void Start()
+        {
+                
+        }
+                
 }
--- unity-final-project/Assets/scripts/Player.cs
+++ unity-final-project/Assets/scripts/Player.cs
@@ -1,5 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 public class Player
 {
-        
+    private double _life;
+    private double _walkVelocity;
+    private double _jumpForce;
+    private List<Weapon> _weapons;
+    private List<Consumable> _consumibles;
+    
 }
